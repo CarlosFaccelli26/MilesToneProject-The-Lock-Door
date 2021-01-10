b2dcb184d0a3898bb6ac3b5735fb713480cebe9c
CarlosFaccelli26 <70590746+CarlosFaccelli26@users.noreply.github.com>
2021-01-10 12:27:16 +0000

    Adding sendmail function on its respective folder

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,4 +1,16 @@
-let templateParams = {
-    name: 'Carlos',
-    notes: 'I would like to know more abour...'
+function sendMail(contactForm) {
+    emailjs.send("service_wmwdfad", "template_jaemmth", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "buy_ticket": contactForm.buyticket.value
+    })
+    .then (
+        function(response) {
+            alert('Message sent succesfully', response)
+        },
+        function(error) {
+            alert('Something went wrong, try again', error)
+        }
+    )
+    return false;
 }
