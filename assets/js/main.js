0acd5c512b32898c17b3a2886737b263e750f22f
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-11-11 20:12:54 +0000

    Orginized main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // Adding smooth scroll behavior
-$(document).ready(function() {
+$(document).ready(function () {
     // Adding smooth scrolling to nav links
-    $('a').on('click', function() {
-        if(this.hash !== "") {
+    $('a').on('click', function () {
+        if (this.hash !== "") {
 
             // Storing hash
             var hash = this.hash;
@@ -10,34 +10,41 @@ $(document).ready(function() {
             // Animation of scrolling behaviour
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 100, function(){
+            }, 100, function () {
                 window.location.hash = hash;
             });
         }
     });
-});
 
-// Changing background color of navar when scrolling
-$(window).scroll(function() {
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 80);
-    $('.nav-link').toggleClass('scrolled', $(this).scrollTop() > 80);
-});
+    // Code taken from https://stackoverflow.com/questions/23706003/changing-nav-bar-color-after-scrolling
+    
+    $(function () {
+        $(document).scroll(function () {
+          var $nav = $(".navbar");
+          $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+      });
 
+    // Creating Accordion for Rules
 
-// Creating Accordion for Rules
-var acc = document.getElementsByClassName('accordion');
-var i;
-    // Bucle For
-for (i = 0; i < acc.length; i++) {
-    // Event listener when i click on accrodion class
-    acc[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-        var panel = this.nextElementSibling;
-        // Conditional If
-        if(panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + 'px';
+    function accordionRules() {
+        var acc = document.getElementsByClassName('accordion');
+        var i;
+        // Bucle For
+        for (i = 0; i < acc.length; i++) {
+            // Event listener when i click on accrodion class
+            acc[i].addEventListener('click', function () {
+                this.classList.toggle('active');
+                var panel = this.nextElementSibling;
+                // Conditional If
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + 'px';
+                }
+            });
         }
-    });
-}
+    }
+
+    accordionRules();
+});
